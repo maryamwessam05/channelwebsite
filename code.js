604b146f3e060d47a83de9c12f51ef73e4264369
Zeina Tamer <23-101312@students.eui.edu.eg>
2025-11-11 14:22:15 +0200

    nav

---
+++ code.js
@@ -0,0 +1,10 @@
+document.getElementById("nav_items").innerHTML += `
+<li class="selected">
+                    <a href="">Home</a>
+                </li>
+                <li>
+                    <a href="">Browse</a>
+                </li>
+                <li>
+                    <a href="">Contact</a>
+                </li>`
